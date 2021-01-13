bb4548551fe9a05dd7a4bd3750717bd51b0e92a5
jack-rainbow <987709173@qq.com>
2021-01-13 10:02:13 +0800

    feat: chart-padding

--- src/components/tree/tooltip.js
+++ src/components/tree/tooltip.js
@@ -30,47 +30,47 @@ export default {
                 }
             }
         },
-        // "options.tooltipPadding": {
-        //     handler() {
-        //         this.tooltipPadding = this.options.tooltipPadding;
-        //         let textWidth;
-        //         let textHeight;
-        //         const padding = this.tooltipPadding;
-        //         const chartpadding = this.chartPadding;
+        "options.tooltipPadding": {
+            handler() {
+                this.tooltipPadding = this.options.tooltipPadding;
+                let textWidth;
+                let textHeight;
+                const padding = this.tooltipPadding;
+                const chartpadding = this.chartPadding;
 
-        //         // 修改鼠标悬浮事件
-        //         this.chart.on("mouseover", function(d) {
-        //             const x = d3.event.layerX - 50 - chartpadding.left;
-        //             const y = d3.event.layerY - chartpadding.top;
-        //             d3.select(".tooltip")
-        //                 .attr("transform", `translate(${x},${y})`)
-        //                 .attr("opacity", 0.7);
-        //             d3.select(".tooltip")
-        //                 .select("text")
-        //                 .text(`${d.name}:${d.value}`)
-        //                 .attr("width", function() {
-        //                     textWidth = this.getComputedTextLength();
-        //                     textHeight = this.getBBox().height;
-        //                     return 14;
-        //                 });
-        //             // 更新tooltip矩形的尺寸
-        //             d3.select(".tooltip")
-        //                 .select("rect")
-        //                 .attr("width", textWidth + padding.left + padding.right)
-        //                 .attr(
-        //                     "height",
-        //                     textHeight + padding.top + padding.bottom
-        //                 );
-        //         });
-        //         this.tooltip
-        //             .select("text")
-        //             .attr(
-        //                 "transform",
-        //                 `translate(${this.tooltipPadding.left},${this
-        //                     .tooltipPadding.top + 12})`
-        //             );
-        //     }
-        // },
+                // 修改鼠标悬浮事件
+                this.chart.on("mouseover", function(d) {
+                    const x = d3.event.layerX - 10 - chartpadding.left;
+                    const y = d3.event.layerY - 230;
+                    d3.select(".tooltip")
+                        .attr("transform", `translate(${x},${y})`)
+                        .attr("opacity", 0.7);
+                    d3.select(".tooltip")
+                        .select("text")
+                        .text(`${d.name}:${d.value}`)
+                        .attr("width", function() {
+                            textWidth = this.getComputedTextLength();
+                            textHeight = this.getBBox().height;
+                            return 14;
+                        });
+                    // 更新tooltip矩形的尺寸
+                    d3.select(".tooltip")
+                        .select("rect")
+                        .attr("width", textWidth + padding.left + padding.right)
+                        .attr(
+                            "height",
+                            textHeight + padding.top + padding.bottom
+                        );
+                });
+                this.tooltip
+                    .select("text")
+                    .attr(
+                        "transform",
+                        `translate(${this.tooltipPadding.left},${this
+                            .tooltipPadding.top + 12})`
+                    );
+            }
+        },
         "options.tooltipColor": {
             handler() {
                 this.tooltip
@@ -96,45 +96,6 @@ export default {
                     .attr("ry", `${this.options.tooltipBorderRadius}`);
             }
         }
-        // 'options.tooltipPadding': {
-        //   handler() {
-        //     this.tooltipPadding = this.options.tooltipPadding
-        //     let textWidth
-        //     let textHeight
-        //     const padding = this.tooltipPadding
-        //     const chartpadding = this.chartPadding
-
-        //     // 修改鼠标悬浮事件
-        //     this.chart.on('mouseover', function (d) {
-        //       const x = d3.event.layerX - 50 - chartpadding.left
-        //       const y = d3.event.layerY - chartpadding.top
-        //       d3.select('.tooltip')
-        //         .attr('transform', `translate(${x},${y})`)
-        //         .attr('opacity', 0.7)
-        //       d3.select('.tooltip')
-        //         .select('text')
-        //         .text(`${d.name}:${d.value}`)
-        //         .attr('width', function () {
-        //           textWidth = this.getComputedTextLength()
-        //           textHeight = this.getBBox().height
-        //           return 14
-        //         })
-        //       // 更新tooltip矩形的尺寸
-        //       d3.select('.tooltip')
-        //         .select('rect')
-        //         .attr('width', textWidth + padding.left + padding.right)
-        //         .attr('height', textHeight + padding.top + padding.bottom)
-        //     })
-        //     this.tooltip
-        //       .select('text')
-        //       .attr(
-        //         'transform',
-        //         `translate(${this.tooltipPadding.left},${
-        //           this.tooltipPadding.top + 12
-        //         })`
-        //       )
-        //   },
-        // },
     },
     methods: {
         initTooltip() {
@@ -169,23 +130,20 @@ export default {
                 .select("rect")
                 .attr("width", 75)
                 .attr("height", 23); // 为提示框设置默认尺寸
-            console.log(this.chart, "----");
 
-            // TODO 下边不生效
-            this.chart.on("mouseover", function(d) {
-                console.log(d, "ddddddddddddd");
-            });
-
+            // 提示框跟随鼠标移动
             d3.select("g")
-                .on("mouseover", function(d) {
-                    console.log(d, "---");
+                .on("mouseover", function() {
+                    const _text = d3.event.target.textContent;
                     if (d3.event.target.nodeName === "text") {
                         const x =
                             d3.event.layerX - 10 - _that.chartpadding.left;
                         const y = d3.event.layerY - 230;
                         d3.select(".tooltip")
                             .attr("opacity", "0.7")
-                            .attr("transform", `translate(${x},${y})`); // 提示框跟随鼠标移动
+                            .attr("transform", `translate(${x},${y})`)
+                            .select("text")
+                            .text(() => _text);
                     }
                 })
                 .on("mouseout", () => {
